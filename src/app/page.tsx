6600c6b9255a8965e5e2b1d74c08eb8d0466dd9f
tim s <t.sorisa@gmail.com>
2025-08-13 21:58:17 -0700

    Created Different Pages, edited layout, added golbal css, created blog and project types

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div > Hi, Developer!</div>
+    <div >Home Page</div>
   );
 }
---
+++ src/app/projects/page.tsx
@@ -0,0 +1,9 @@
+import { div } from "framer-motion/client"
+
+const Projects = () => {
+    return (
+        <div> Project Page</div>
+    )
+}
+
+export default Projects;
